4e6b2e8f15735c03c36eda420288d0e6bb0ee465
Morten W. Petersen <morphex@gmail.com>
2023-04-16 21:27:21 +0200

    Creating working examples

--- examples/emacs-custom.sh
+++ examples/emacs-custom.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 old_directory=$PWD
-cd /home/morphex
+cd $HOME
+./simple-lock-manager/get_lock.py emacs
+if [ $? -ne 0 ]; then
+    echo "Error, exiting"
+    exit 1
+fi
 if [[ ! -L .emacs ]]; then
     echo ".emacs not a symbolic link, exiting."
     exit 1
@@ -12,3 +17,4 @@ ln -s .emacs-custom .emacs
 
 cd $old_directory
 emacs "$@"
+$HOME/simple-lock-manager/remove_lock.py emacs
